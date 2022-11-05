62e6310c54460d00c8f0838c27adb41d22f0e116
rahulbishnoi58 <rahuldelu58@gmail.com>
2022-11-05 11:51:33 +0530

    first commit

--- dbConnect.js
+++ dbConnect.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://adminrahul:rahulbhai@cluster0.cl4k7hi.mongodb.net/Expense_Tracker', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://adminrahul:Rahul123@cluster0.cl4k7hi.mongodb.net/Expense_Tracker' , {useNewUrlParser : true , useUnifiedTopology : true})
 
-const connection = mongoose.connection;
+const connection = mongoose.connection
 
-connection.on('error', err => console.log(err));
+connection.on('error', err => console.log(err))
 
-connection.on('connected', () => console.log('Mongo Db Connection Successfull'));
+connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const User = require('../models/User')
-const router = express.Router()
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
 
-router.post('/login',async function(req,res){
-    try {
-        const result = await User.findOne({email : req.body.email , password: req.body.password})
-    if(result){
-        res.send(result);
-    }else{
-        res.status(500).json('Error');
+router.post("/login", async function (req, res) {
+  try {
+    const result = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    if (result) {
+      res.send(result);
+    } else {
+      res.status(500).json("Error");
     }
-        
-    } catch (error) {
-        res.status(500).json(error);
-    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-
-router.post('/register',async function(req,res){
+router.post("/register", async function (req, res) {
     try {
-
-       const newuser = new User(req.body);
-       await newuser.save();
-       res.send('User Registered Successfully')
-        
+      const newuser = new User(req.body);
+      await newuser.save();
+      res.send('User Registered Successfully')
     } catch (error) {
-        res.status(500).json(error);
+      res.status(500).json(error);
     }
-});
+  });
 
 
-module.exports = router;
+  module.exports = router;
